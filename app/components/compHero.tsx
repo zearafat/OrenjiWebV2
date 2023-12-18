64a6aa619266528edf7ea4291a85c160c6b75b08
zeaarafat <alzea.arafat@icloud.com>
2023-12-18 11:10:09 +0700

    Change hero cta copy

--- app/components/compHero.tsx
+++ app/components/compHero.tsx
@@ -45,10 +45,10 @@ export default function compHero() {
                         <div className={"sm:py-4"}>
                             <Link href={"https://tally.so/r/wkNrkZ"} target={"_blank"}>
                                 <button type="button"
-                                        className={"sm:px-[62px] sm:py-[22px] py-4 px-14 sm:text-xl text-lg tracking-tight font-medium text-center " +
+                                        className={"sm:px-[32px] sm:py-[22px] py-4 px-14 sm:text-xl text-lg tracking-tight font-medium text-center " +
                                             "btn-gradient-orenji hover:shadow-xl hover:shadow-orange-200/50 hover:-translate-y-1 " +
                                             "hover:scale-100 duration-300"}>
-                                    Start a project
+                                    Start your dream project
                                 </button>
                             </Link>
                             <p className={"pt-8"}>
